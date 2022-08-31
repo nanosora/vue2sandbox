80777af5b9e90e212c0f88e295b519732246af08
naoking <naoki@zc4.so-net.ne.jp>
2022-08-31 17:07:03 +0000

    Update App.vue and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,92 @@
 <template>
   <div id="app">
+    <el-alert
+      v-for="item in errors"
+      :key="item"
+      :title="item.msg"
+      type="error"
+      show-icon
+    ></el-alert>
+    <label>simei</label>
+    <el-input
+      id="simei"
+      name="simei"
+      size="small"
+      placeholder="simei"
+      v-model="model.simei"
+      maxlength="10"
+      show-word-limit
+      clearable
+      data-vv-as="氏名"
+      v-validate="'length_sum:' + isLngthOver"
+    ></el-input>
+    <label>kaisya</label>
+    <el-input
+      id="kaisya"
+      namme="kaisya"
+      size="small"
+      placeholder="kaisya"
+      v-model="model.kaisya"
+      maxlength="10"
+      show-word-limit
+      clearable
+      data-vv-as="会社名"
+      v-validate="'length_sum:' + isLngthOver"
+    ></el-input>
+    <label>busho</label>
+    <el-input
+      id="busho"
+      name="busho"
+      size="small"
+      placeholder="busho"
+      v-model="model.busho"
+      maxlength="10"
+      show-word-limit
+      clearable
+      data-vv-as="部署名"
+      v-validate="'length_sum:' + isLngthOver"
+    ></el-input>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
-  components: {
-  },
+  components: {},
   data: () => ({
+    model: {
+      simei: "",
+      kaisya: "",
+      busho: "",
+    },
   }),
+  created() {
+    this.$validator.extend("length_sum", {
+      getMessage(field) {
+        return `${field}：3項目合計で10文字以内で入力してください`;
+      },
+      validate(value, [otherVal]) {
+        console.log(otherVal);
+        if (JSON.parse(otherVal.toLowerCase())) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+    });
+  },
+  computed: {
+    isLngthOver() {
+      const len1 = this.model.simei.length;
+      const len2 = this.model.kaisya.length;
+      const len3 = this.model.busho.length;
+      if (len1 + len2 + len3 > 10) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 </script>
 
@@ -19,7 +95,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import VeeValidate, { Validator } from "vee-validate";
 import ja from "vee-validate/dist/locale/ja";
 
 Validator.localize("ja", ja);
 Vue.use(VeeValidate, {
-  locale: "ja"
+  locale: "ja",
 });
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
